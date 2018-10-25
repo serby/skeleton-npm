fd6a94c373390820017226f84d21fe153dbf0c9a
Paul Serby <paul.serby@clock.co.uk>
2018-10-25 23:45:29 +0100

    Remove instructions from readme

--- go.sh
+++ go.sh
@@ -6,6 +6,9 @@ read -e -p "Description of package: " description
 sed -i -e "s/{NAME}/$name/g" Readme.md package.json
 sed -i -e "s/{DESCRIPTION}/$description/g" Readme.md package.json
 
+cat Readme.md | sed '27,$d' > Readme2.md
+mv Readme2.md Readme.md
+
 rm *.*-e
 rm go.sh
 rm -rf .git
